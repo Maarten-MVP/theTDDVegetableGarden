912148a1c0f3e979ecd9dcd4c1490b5da7aff0fd
Maarten Van Poucke <maarten.v.poucke@gmail.com>
2022-02-01 07:06:25 +0100

    part 2

--- farm.js
+++ farm.js
@@ -1,24 +1,60 @@
-const getYieldForPlant = object => object.yield;
+const getYieldForPlant = crop => crop.yield;
 
-const getYieldForCrop = object => object.numCrops * object.crop.yield;
+const getYieldForCrop = crop => crop.numCrops * crop.crop.yield;
 
-const getTotalYield = (crops) => {
+const getTotalYield = crops => {
 
-    const yieldArray = crops.crops;
+    const cropsArray = crops.crops;
 
-    const totalYiel = yieldArray.map((item) => {
-        return getYieldForCrop(item)
-    }).reduce((previousValue, currentValue) => {
-        return previousValue + currentValue
-        });
-    return getTotalYield;
+    const totalYield = cropsArray
+        .map(item => getYieldForCrop(item))
+        .reduce((previousValue, currentValue) => previousValue + currentValue);
+    return totalYield;
+};
+
+const getCostForCrop = crops => {
+
+    const totalYield = getTotalYield(crops);
+
+    const cropCost = crops.crops.map(item => item.crop.cost);
+    return totalYield * cropCost;
 }
 
+const getRevenueForCrop = crops => {
 
+    const totalYield = getTotalYield(crops);
+    const cropPrice = crops.crops.map(item => item.crop.price);
+    return totalYield * cropPrice;
+}
+
+const getProfitForCrop = crops => getRevenueForCrop(crops) - getCostForCrop(crops);
+
+const getTotalProfit = crops => {
+
+    const cropsArray = crops.crops;
+
+    console.log(cropsArray)
+
+    const totalCost = cropsArray
+        .map(item => (getYieldForCrop(item) * item.crop.cost))
+        .reduce((previousValue, currentValue) => previousValue + currentValue);
+    console.log(totalCost)
+
+    const totalRevenue = cropsArray
+        .map (item => (getYieldForCrop(item) * item.crop.price))
+        .reduce((previousValue, currentValue) => previousValue + currentValue);
+    console.log(totalRevenue)
+
+    return totalRevenue - totalCost;
+};
 
 module.exports = {
     getYieldForPlant,
     getYieldForCrop,
-    getTotalYield
+    getTotalYield,
+    getCostForCrop,
+    getRevenueForCrop,
+    getProfitForCrop,
+    getTotalProfit,
 };
 
--- farm.test.js
+++ farm.test.js
@@ -1,4 +1,12 @@
-const { getYieldForPlant, getYieldForCrop, getTotalYield } = require("./farm");
+const { 
+    getYieldForPlant, 
+    getYieldForCrop, 
+    getTotalYield,
+    getCostForCrop,
+    getRevenueForCrop,
+    getProfitForCrop,
+    getTotalProfit,
+} = require("./farm");
 
 describe("getYieldForPlant", () => {
     const corn = {
@@ -51,3 +59,78 @@ describe("getTotalYield", () => {
         expect(getTotalYield({ crops })).toBe(0);
     });
 });
+
+describe("getCostForCrop", () => {
+    test("Calculate total cost of a crop", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+            cost: 1,
+        };
+
+        const crops = [{ crop: corn, numCrops: 5 }];
+        expect(getCostForCrop({ crops })).toBe(15);
+    });
+});
+
+describe("getRevenueForCrop", () => {
+    test("Calculate total revenue of a crop", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+            cost: 1,
+            price: 3,
+        };
+
+        const crops = [{ crop: corn, numCrops: 5 }];
+        expect(getRevenueForCrop({ crops })).toBe(45);
+    });
+});
+
+describe("getProfitForCrop", () => {
+    test("Calculate total profit of a crop", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+            cost: 1,
+            price: 3,
+        };
+
+        const crops = [{ crop: corn, numCrops: 5 }];
+        expect(getProfitForCrop({ crops })).toBe(30);
+    });
+});
+
+describe("getTotalProfit", () => {
+    test("Calculate total profit with multiple crops", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+            cost: 1,
+            price: 3,
+        };
+        const pumpkin = {
+            name: "pumpkin",
+            yield: 4,
+            cost: 1.5,
+            price: 4.5,
+        };
+        const crops = [
+            { crop: corn, numCrops: 5 },
+            { crop: pumpkin, numCrops: 2 },
+        ];
+        expect(getTotalProfit({ crops })).toBe(54);
+    });
+
+    test("Calculate total profit with 0 amount", () => {
+        const corn = {
+            name: "corn",
+            yield: 3,
+            cost: 1,
+            price: 3,
+        };
+        const crops = [{ crop: corn, numCrops: 0 }];
+        expect(getTotalProfit({ crops })).toBe(0);
+    });
+});
+
